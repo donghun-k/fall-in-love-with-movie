b950dddf7d82197b4546d6237ef5fb5e610de823
DongHun, Kim <donghun.kdh@gmail.com>
2023-09-23 16:58:34 +0900

    :lipstick: MUI 테마 프로바이더 설정 및 기본 레이아웃 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from './queryClient';
 import { Outlet } from 'react-router-dom';
+import { CssBaseline, Grid, ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Outlet />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Grid
+          container
+          sx={{
+            background: '#000000',
+            width: '100vw',
+            height: '100vh',
+          }}
+        >
+          <Grid item xs={0} md={1} lg={2} />
+          <Grid item xs={12} md={10} lg={8}>
+            <Outlet />
+          </Grid>
+          <Grid item xs={0} md={1} lg={2} />
+        </Grid>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FE8BAB',
+    },
+    secondary: {
+      main: '#303030',
+    },
+  },
+});
+
+export default theme;
