52031f94479ceee7128184ad6ed2ad1063916536
DongHun, Kim <donghun.kdh@gmail.com>
2023-09-25 15:57:48 +0900

    :wrench: 파이어베이스 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import queryClient from './queryClient';
+import queryClient from './configs/queryClient';
 import { Outlet } from 'react-router-dom';
 import {
   CssBaseline,
@@ -10,7 +10,7 @@ import {
 } from '@mui/material';
 import NavBar from './components/layout/nav-bar/NavBar';
 import { useCallback, useMemo, useState } from 'react';
-import getDesignTokens from './theme';
+import getDesignTokens from './configs/theme';
 import TabBar from './components/layout/tab-bar/TabBar';
 
 function App() {
---
+++ src/configs/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_DB_URL,
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  databaseURL: VITE_FIREBASE_DB_URL,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
