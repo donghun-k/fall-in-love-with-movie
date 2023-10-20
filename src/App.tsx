496ccb0fab6df2406c9c122cae87463f9f9d66e1
DongHun, Kim <donghun.kdh@gmail.com>
2023-10-20 20:46:00 +0900

    :recycle: 레이아웃 코드 Layout 컴포넌트로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,17 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from './configs/queryClient';
 import { Outlet } from 'react-router-dom';
-import {
-  CssBaseline,
-  Grid,
-  PaletteMode,
-  ThemeProvider,
-  createTheme,
-} from '@mui/material';
-import NavBar from './components/layout/nav/NavBar';
-import BottomNavBar from './components/layout/bottom-nav/BottomNavBar';
-import { useCallback, useMemo, useState } from 'react';
-import getDesignTokens from './configs/theme';
 import { AuthContextProvider } from './contexts/AuthContext';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import SearchDialog from './components/layout/SearchDialog';
+import Layout from './components/layout/Layout';
 
 function App() {
-  const [mode, setMode] = useState<PaletteMode>('dark');
-  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
-  const togglePaletteMode = useCallback(() => {
-    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-  }, []);
-  const [openSearchBox, setOpenSearchBox] = useState(false);
-  const handleSearchDialogOpen = () => {
-    setOpenSearchBox(true);
-  };
-  const handleSearchDialogClose = () => {
-    setOpenSearchBox(false);
-  };
   return (
     <AuthContextProvider>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <NavBar
-            togglePaletteMode={togglePaletteMode}
-            handleSearchDialogOpen={handleSearchDialogOpen}
-          />
-          <Grid
-            container
-            sx={{
-              background: 'background.default',
-              width: '100vw',
-              minHeight: {
-                xs: 'calc(100vh - 160px)',
-                sm: 'calc(100vh - 80px)',
-              },
-            }}
-          >
-            <Grid item xs={0} md={1} lg={2} />
-            <Grid item xs={12} md={10} lg={8} position="relative">
-              <Outlet />
-            </Grid>
-            <Grid item xs={0} md={1} lg={2} />
-          </Grid>
-          <BottomNavBar handleSearchDialogOpen={handleSearchDialogOpen} />
-          <SearchDialog
-            open={openSearchBox}
-            handleSearchDialogClose={handleSearchDialogClose}
-          />
-        </ThemeProvider>
+        <Layout>
+          <Outlet />
+        </Layout>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </AuthContextProvider>
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,94 @@
+import {
+  CssBaseline,
+  Grid,
+  PaletteMode,
+  Theme,
+  ThemeProvider,
+  createTheme,
+} from '@mui/material';
+import { ReactNode, useCallback, useMemo, useState } from 'react';
+import getDesignTokens from '../../configs/theme';
+import BottomNavBar from './bottom-nav/BottomNavBar';
+import SearchDialog from './common/SearchDialog';
+import NavBar from './nav/NavBar';
+
+interface Props {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  const [mode, setMode] = useState<PaletteMode>('dark');
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+  const togglePaletteMode = useCallback(() => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  }, []);
+
+  const [openSearchBox, setOpenSearchBox] = useState(false);
+  const handleSearchDialogOpen = () => {
+    setOpenSearchBox(true);
+  };
+  const handleSearchDialogClose = () => {
+    setOpenSearchBox(false);
+  };
+  const props = {
+    children,
+    theme,
+    togglePaletteMode,
+    handleSearchDialogOpen,
+    handleSearchDialogClose,
+    openSearchBox,
+  };
+  return <LayoutView {...props} />;
+};
+
+interface ViewProps {
+  children: ReactNode;
+  theme: Theme;
+  togglePaletteMode: () => void;
+  handleSearchDialogOpen: () => void;
+  handleSearchDialogClose: () => void;
+  openSearchBox: boolean;
+}
+
+const LayoutView = ({
+  children,
+  theme,
+  togglePaletteMode,
+  handleSearchDialogOpen,
+  handleSearchDialogClose,
+  openSearchBox,
+}: ViewProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar
+        togglePaletteMode={togglePaletteMode}
+        handleSearchDialogOpen={handleSearchDialogOpen}
+      />
+      <Grid
+        container
+        sx={{
+          background: 'background.default',
+          width: '100vw',
+          minHeight: {
+            xs: 'calc(100vh - 160px)',
+            sm: 'calc(100vh - 80px)',
+          },
+        }}
+      >
+        <Grid item xs={0} md={1} lg={2} />
+        <Grid item xs={12} md={10} lg={8} position="relative">
+          {children}
+        </Grid>
+        <Grid item xs={0} md={1} lg={2} />
+      </Grid>
+      <BottomNavBar handleSearchDialogOpen={handleSearchDialogOpen} />
+      <SearchDialog
+        open={openSearchBox}
+        handleSearchDialogClose={handleSearchDialogClose}
+      />
+    </ThemeProvider>
+  );
+};
+
+export default Layout;
