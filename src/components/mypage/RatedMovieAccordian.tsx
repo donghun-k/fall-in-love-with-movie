02e1d8941f0ade244d8fb1004aff9cfdd3538587
DongHun, Kim <donghun.kdh@gmail.com>
2023-10-25 00:27:26 +0900

    :recycle: 평가한 영화 정렬 기준 Menu > Select로 변경

--- src/components/mypage/RatedMovieAccordian.tsx
+++ src/components/mypage/RatedMovieAccordian.tsx
@@ -3,20 +3,17 @@ import {
   AccordionDetails,
   AccordionSummary,
   Box,
-  Button,
   Chip,
   Divider,
   List,
   ListItem,
   ListItemButton,
-  Menu,
   MenuItem,
   Pagination,
   Select,
   SelectChangeEvent,
   Typography,
 } from '@mui/material';
-import SortIcon from '@mui/icons-material/Sort';
 import StarIcon from '@mui/icons-material/Star';
 import Rating from '../../types/Rating';
 import { ChangeEvent, useEffect, useState } from 'react';
@@ -32,12 +29,9 @@ type SortOption = '최신순' | '등록순' | '가나다순';
 const RatedMovieAccordian = ({ ratings }: Props) => {
   const navigate = useNavigate();
   const [ratingValueToShow, setRatingValueToShow] = useState<number>(0);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [sortOption, setSortOption] = useState<SortOption>('가나다순');
   const [sortedRatings, setSortedRatings] = useState<Rating[]>([]);
   const [page, setPage] = useState<number>(1);
-
-  const openSortMenu = Boolean(anchorEl);
 
   useEffect(() => {
     let sortedRatings = [...ratings];
@@ -64,18 +58,6 @@ const RatedMovieAccordian = ({ ratings }: Props) => {
     setSortedRatings(sortedRatings);
   }, [ratings, sortOption, ratingValueToShow]);
 
-  const handleSortMenuButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(e.currentTarget);
-  };
-  const handleSortMenuClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleSetSortOption = (e: MouseEvent<HTMLLIElement>) => {
-    setSortOption(e.currentTarget.textContent as SortOption);
-    setAnchorEl(null);
-  };
-
   const handleSetPage = (_: ChangeEvent<unknown>, page: number) => {
     setPage(page);
   };
@@ -86,18 +68,18 @@ const RatedMovieAccordian = ({ ratings }: Props) => {
     navigate(`/movie/${movieId}`);
   };
 
+  const handleSetSortOption = (e: SelectChangeEvent) => {
+    setSortOption(e.target.value as SortOption);
+  };
+
   const handleSetRatingValueToShow = (e: SelectChangeEvent) => {
     setRatingValueToShow(Number(e.target.value));
   };
 
   const props = {
     sortOption,
-    sortedRatings,
-    openSortMenu,
-    anchorEl,
-    handleSortMenuButtonClick,
-    handleSortMenuClose,
     handleSetSortOption,
+    sortedRatings,
     page,
     handleSetPage,
     handleToMovieDetailPage,
@@ -109,12 +91,8 @@ const RatedMovieAccordian = ({ ratings }: Props) => {
 
 interface ViewProps {
   sortOption: SortOption;
+  handleSetSortOption: (e: SelectChangeEvent) => void;
   sortedRatings: Rating[];
-  openSortMenu: boolean;
-  anchorEl: HTMLElement | null;
-  handleSortMenuButtonClick: (e: MouseEvent<HTMLButtonElement>) => void;
-  handleSortMenuClose: () => void;
-  handleSetSortOption: (e: MouseEvent<HTMLLIElement>) => void;
   page: number;
   handleSetPage: (e: ChangeEvent<unknown>, page: number) => void;
   handleToMovieDetailPage: (e: MouseEvent<HTMLElement>) => void;
@@ -124,12 +102,8 @@ interface ViewProps {
 
 const RatedMovieAccordianView = ({
   sortOption,
-  sortedRatings,
-  openSortMenu,
-  anchorEl,
-  handleSortMenuButtonClick,
-  handleSortMenuClose,
   handleSetSortOption,
+  sortedRatings,
   page,
   handleSetPage,
   handleToMovieDetailPage,
@@ -159,27 +133,9 @@ const RatedMovieAccordianView = ({
             height: '40px',
           }}
         >
-          <Button
-            sx={{
-              fontWeight: 'bold',
-            }}
-            startIcon={<SortIcon />}
-            onClick={handleSortMenuButtonClick}
-          >
-            {sortOption}
-          </Button>
-          <Menu
-            anchorEl={anchorEl}
-            open={openSortMenu}
-            onClose={handleSortMenuClose}
-          >
-            <MenuItem onClick={handleSetSortOption}>가나다순</MenuItem>
-            <MenuItem onClick={handleSetSortOption}>최신순</MenuItem>
-            <MenuItem onClick={handleSetSortOption}>등록순</MenuItem>
-          </Menu>
           <Select
-            value={String(ratingValueToShow)}
-            onChange={handleSetRatingValueToShow}
+            value={String(sortOption)}
+            onChange={handleSetSortOption}
             sx={{
               width: '100px',
               height: '40px',
@@ -187,6 +143,30 @@ const RatedMovieAccordianView = ({
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
+              },
+              '& .MuiOutlinedInput-notchedOutline': {
+                border: 'none',
+              },
+            }}
+          >
+            <MenuItem value="가나다순">가나다순</MenuItem>
+            <MenuItem value="최신순">최신순</MenuItem>
+            <MenuItem value="등록순">등록순</MenuItem>
+          </Select>
+          <Select
+            value={String(ratingValueToShow)}
+            onChange={handleSetRatingValueToShow}
+            sx={{
+              width: '80px',
+              height: '40px',
+              '& .MuiSelect-select': {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                border: 'none',
+              },
+              '& .MuiOutlinedInput-notchedOutline': {
+                border: 'none',
               },
             }}
           >
