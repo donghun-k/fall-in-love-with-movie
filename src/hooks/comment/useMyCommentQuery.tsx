3e28d2f918a6066c9cf2d086d4fc8777875ab6a6
DongHun, Kim <donghun.kdh@gmail.com>
2023-11-02 14:35:13 +0900

    :bug: 영화 상세 정보 페이지에서 로그인 했을 때 유저 별점 및 코멘트 정보 표시 오류 수정

--- src/hooks/comment/useMyCommentQuery.tsx
+++ src/hooks/comment/useMyCommentQuery.tsx
@@ -1,18 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import { getMyComment } from '../../api/comment';
+import useAuthContext from '../useAuthContext';
 
 interface Params {
   movieId: number;
 }
 
 const useMyCommentQuery = ({ movieId }: Params) => {
+  const { user } = useAuthContext();
   return useQuery(
-    ['myComment', movieId],
+    ['myComment', movieId, user?.uid],
     () => {
       return getMyComment({ movieId });
     },
     {
-      enabled: !!movieId,
+      enabled: !!movieId && !!user,
       refetchOnWindowFocus: false,
       staleTime: 1000 * 60 * 60 * 24,
     }
--- src/hooks/rating/useMyRatingQuery.tsx
+++ src/hooks/rating/useMyRatingQuery.tsx
@@ -1,18 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import { getMyRating } from '../../api/rating';
+import useAuthContext from '../useAuthContext';
 
 interface Params {
   movieId: number;
 }
 
 const useMyRatingQuery = ({ movieId }: Params) => {
+  const { user } = useAuthContext();
   return useQuery(
-    ['myRating', movieId],
+    ['myRating', movieId, user?.uid],
     () => {
       return getMyRating({ movieId });
     },
     {
-      enabled: !!movieId,
+      enabled: !!movieId && !!user,
       refetchOnWindowFocus: false,
       staleTime: 1000 * 60 * 60 * 24,
     }
