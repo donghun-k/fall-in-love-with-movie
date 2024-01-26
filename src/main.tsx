9ecca2faf044de909665bf29e876c26f4e84ca63
DongHun, Kim <donghun.kdh@gmail.com>
2024-01-27 05:43:22 +0900

    :recycle: Main.tsx에서 바로 QueryClientProvider 사용하도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,22 @@ import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { store } from './store';
-import TanstackQueryContextProvider from './contexts/TanstackQueryContext';
 import App from './App';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <TanstackQueryContextProvider>
+      <QueryClientProvider client={queryClient}>
         <HelmetProvider>
           <App />
         </HelmetProvider>
         <ReactQueryDevtools initialIsOpen={false} />
-      </TanstackQueryContextProvider>
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
