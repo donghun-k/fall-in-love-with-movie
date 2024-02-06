99a97827d29a2f301269b1d651d302604bc1841b
DongHun, Kim <donghun.kdh@gmail.com>
2024-02-07 00:27:00 +0900

    :recycle: 타임스탬프 문자열 변환 함수 date-fns 사용

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,11 @@
+import { format } from 'date-fns';
+import { ko } from 'date-fns/locale';
+
 export const convertTimestampToDateString = (timestamp: number) => {
-  return new Date(timestamp).toLocaleDateString('ko-KR', {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  });
+  const date = new Date(timestamp);
+  if (Number.isNaN(date.getTime())) {
+    throw new Error('유효하지 않은 타임스탬프입니다.');
+  }
+
+  return format(date, 'yyyy. MM. dd. a hh:mm', { locale: ko });
 };
