bb9ac084e087d74da44724e9ed17a39ac9e6a205
DongHun, Kim <donghun.kdh@gmail.com>
2024-03-26 23:02:44 +0900

    :fire: 'package.json'에서 'type': 'module' 설정 삭제

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
 
@@ -9,16 +9,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // SEARCH MOVIE ROUTE
-app.get("/search/movie", async (req: Request, res: Response) => {
+app.get('/search/movie', async (req: Request, res: Response) => {
   const { query, page } = req.query;
 
   try {
     const response = await fetch(
       `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=true&language=ko-KR&page=${page}`,
       {
-        method: "GET",
+        method: 'GET',
         headers: {
-          accept: "application/json",
+          accept: 'application/json',
           Authorization: `Bearer ${VITE_TMDB_ACCESS_TOKEN}`,
         },
       },
@@ -26,21 +26,21 @@ app.get("/search/movie", async (req: Request, res: Response) => {
     const json = await response.json();
     res.json(json);
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
 // GET MOVIE DETAIL ROUTE
-app.get("/movie/:movieId", async (req: Request, res: Response) => {
+app.get('/movie/:movieId', async (req: Request, res: Response) => {
   const { movieId } = req.params;
 
   try {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/${movieId}?language=ko-KR`,
       {
-        method: "GET",
+        method: 'GET',
         headers: {
-          accept: "application/json",
+          accept: 'application/json',
           Authorization: `Bearer ${VITE_TMDB_ACCESS_TOKEN}`,
         },
       },
@@ -48,21 +48,21 @@ app.get("/movie/:movieId", async (req: Request, res: Response) => {
     const json = await response.json();
     res.json(json);
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
 // GET SIMILAR MOVIES ROUTE
-app.get("/movie/:movieId/similar", async (req: Request, res: Response) => {
+app.get('/movie/:movieId/similar', async (req: Request, res: Response) => {
   const { movieId } = req.params;
 
   try {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/${movieId}/similar?language=ko-KR`,
       {
-        method: "GET",
+        method: 'GET',
         headers: {
-          accept: "application/json",
+          accept: 'application/json',
           Authorization: `Bearer ${VITE_TMDB_ACCESS_TOKEN}`,
         },
       },
@@ -70,7 +70,7 @@ app.get("/movie/:movieId/similar", async (req: Request, res: Response) => {
     const json = await response.json();
     res.json(json);
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
